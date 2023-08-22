cdc1e80df60bf2734bb86c6d9173b6416becf4e4
Diana Bautista <bagdiana03@gmail.com>
2023-08-22 17:17:24 -0600

    cambio color

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
-              seedColor: Color.fromARGB(255, 159, 137, 228)),
+              seedColor: Color.fromARGB(255, 40, 0, 159)),
         ),
         home: MyHomePage(),
       ),
