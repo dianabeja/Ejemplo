202d274f17f1dbc19b4605187b131862f9af84ac
Diana Bautista <bagdiana03@gmail.com>
2023-11-03 00:39:48 -0600

    add branch modelotaxi

--- lib/main.dart
+++ lib/main.dart
@@ -25,29 +25,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   String _respuesta = '';
+  int? distance, passengers, miscellaneousfees, tripduration;
+  TextEditingController _urlController = TextEditingController();
+  TextEditingController _shaController = TextEditingController();
 
   Future<void> _consultarModelo() async {
-    try {
-      final body = json.decode(_controller.text);
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
 
-     final url = Uri.parse('https://heart-ml-service-dianabeja.cloud.okteto.net/score');
-      final response = await http.post(url, body: json.encode(body), headers: {"Content-Type": "application/json"});
+      final url =
+          Uri.parse('https://modelotaxi-service-dianabeja.cloud.okteto.net/');
+      final response = await http.post(url,
+          body: json.encode({
+            "distance_traveled": distance,
+            "num_of_passengers": passengers,
+            "miscellaneous_fees": miscellaneousfees,
+            "trip_duration": tripduration
+          }),
+          headers: {"Content-Type": "application/json"});
 
       if (response.statusCode == 200) {
+        Map<String, dynamic> jsonResponse = json.decode(response.body);
+        double? price = jsonResponse['premiumPrice'];
         setState(() {
-          _respuesta = response.body;
+          _respuesta =
+              ' ${price?.toStringAsFixed(2)}'; // Convertimos el número a String con dos decimales
         });
       } else {
         setState(() {
-          _respuesta = 'Error al obtener respuesta';
+          _respuesta =
+              'Error al obtener respuesta, revisa que todos los campos sean validos';
         });
       }
-    } catch (error) {
-      
+    }
+  }
+
+  Future<void> _llamadoAPI() async {
+    final url = Uri.parse(
+        'https://api.github.com/repos/dianabeja/ModeloTaxi/dispatches');
+
+    final body = json.encode({
+      "event_type": "ml_ci_cd",
+      "client_payload": {
+        "dataseturl":
+            _urlController.text, // Obtén el valor desde el controlador
+        "sha": _shaController.text, // Obtén el valor desde el controlador
+      }
+    });
+
+    final headers = {
+      'Authorization': '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',
+      'Accept': 'application/vnd.github.v3+json',
+      'Content-type': 'application/json',
+    };
+
+    final response = await http.post(url, body: body, headers: headers);
+
+    if (response.statusCode == 204) {
       setState(() {
-        _respuesta = 'Error al formatear el JSON';
+        _respuesta = 'Llamado a API exitoso';
+      });
+    } else {
+      setState(() {
+        _respuesta = 'Error al hacer el llamado a la API';
       });
     }
   }
@@ -56,29 +98,118 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Diana App"),
+        title: const Text("Axolot Machine Learning"),
+        backgroundColor: Color.fromARGB(255, 164, 136, 255),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: 'Ingresa tu JSON aquí',
-                border: OutlineInputBorder(),
-              ),
-              maxLines: 10,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Distancia'),
+                    keyboardType: TextInputType.number,
+                    onSaved: (value) => distance = int.tryParse(value ?? ''),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Pasajeros'),
+                    keyboardType: TextInputType.number,
+                    onSaved: (value) => passengers = int.tryParse(value ?? ''),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Gastos'),
+                    keyboardType: TextInputType.number,
+                    onSaved: (value) =>
+                        miscellaneousfees = int.tryParse(value ?? ''),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration:
+                        InputDecoration(labelText: 'Duración del viaje'),
+                    keyboardType: TextInputType.number,
+                    onSaved: (value) =>
+                        tripduration = int.tryParse(value ?? ''),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _consultarModelo,
+                  child: const Text('Consultar Modelo'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 164, 136, 255),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 125, vertical: 22),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 16.0),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 164, 136, 255),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    'Predicción: $_respuesta',
+                    style: TextStyle(
+                      color:
+                          Colors.white, // Texto en blanco para mejor contraste
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    controller:
+                        _shaController, // Asociar el controlador al campo
+                    decoration: InputDecoration(labelText: 'Nombre'),
+                    onSaved: (value) {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    controller:
+                        _urlController, // Asociar el controlador al campo
+                    decoration: InputDecoration(labelText: 'URL'),
+                    // No establecer el keyboardType, ya que es un campo de URL
+                    onSaved: (value) {},
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _llamadoAPI,
+                  child: const Text('Reentrenar Modelo'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 164, 136, 255),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 125, vertical: 22),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(_respuesta),
+              ],
             ),
           ),
-          ElevatedButton(
-            child: Text('Consultar Modelo'),
-            onPressed: _consultarModelo,
-          ),
-          SizedBox(height: 20),
-          Text(_respuesta),
-        ],
+        ),
       ),
     );
   }
